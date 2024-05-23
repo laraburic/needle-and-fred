896f5114a2e73cb8cbcf971d9a1ac38f80d89560
Mike O <mike413318@gmail.com>
2024-05-23 14:06:43 +0100

    Mike's_Drag&DropandZombie

---
+++ Needle and Fred/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public static bool mouseButtonReleased;
+    private Vector3 GetMousePos()
+    {
+        return Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        mousePosition = Input.mousePosition - GetMousePos();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        mouseButtonReleased = true;
+    }
+
+    //https://www.youtube.com/watch?v=9-ok9Cn3d90
+
+    /*private void OnTriggerStay(Collider collison)
+    {
+        string thisGameObjectName;
+        string collisionGameobjectName;
+
+        thisGameObjectName = gameObject.Substring(0, name.IndexOf("_"));
+        collisionGameobjectName = collisiion.gameObject.name.Substring(0, name.IndexOf("_"));
+
+        if (mouseButtonReleased && thisGameObjectName == "Acorn" && thisGameObjectName == collisionGameobjectName
+        {
+            Instantiate(Resources.Load("Oak_Object")), transform.position, Quaternion.identity);
+            mouseButtonReleased= false;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        else if (mouseButtonReleased && thisGameObjectName == "Oak" && thisGameObjectName == collisionGameobjectName
+        {
+            Instantiate(Resources.Load("Rocket_Object")), transform.position, Quaternion.identity);
+            mouseButtonReleased = false;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }*/
+}
