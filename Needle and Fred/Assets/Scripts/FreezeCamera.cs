6b084e47f4761bd6573d5810b79a3df46d997c51
SarahPandaCatDev <165550798+SarahPandaCatDev@users.noreply.github.com>
2024-05-26 15:34:11 +0100

    Made it so when you click on the scroll, the letter opens and added it to the start as well, added a bool to prevent triggering whilst editing
    - The ScrollUI is disabled by default when in Editor/Play
    - The ScrollUI is ENABLED by default when playing via a build
    - You can use the ScrollManager to control whether or not the ScrollUI is active in Editor/Play or not for testing game flow
    - When you click on the Scroll on the table, it will open the scroll to read the letter from Gladis

--- Needle and Fred/Assets/Scripts/FreezeCamera.cs
+++ Needle and Fred/Assets/Scripts/FreezeCamera.cs
@@ -6,18 +6,21 @@ using UnityEngine;
 public class FreezeCamera : MonoBehaviour
 {
     private CinemachineVirtualCamera vCamera;
-    private float originalNoiseValue = 0.3f;
     
     void Start() {
         // Find the camera and turn off the noise
+        DeactivateNoise();
+    }
+
+    public void DeactivateNoise() {
         if (GameObject.FindObjectOfType<CinemachineVirtualCamera>() != null) {
             vCamera = GameObject.FindObjectOfType<CinemachineVirtualCamera>(); // find the camera
-            originalNoiseValue = vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain; // store the original noise value
             vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0f; // turn off shake
         }
     }
 
     public void ReactivateNoise() {
-        vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = originalNoiseValue;
+        vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0.3f;
+        vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 0.7f;
     }
 }
---
+++ Needle and Fred/Assets/ScrollManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ScrollManager : MonoBehaviour
+{
+    private Camera playerCamera;
+    public bool dontTriggerAtStart = false;
+    public GameObject scrollUI;
+    public FreezeCamera freezeCam;
+    public LayerMask scrollLayer; // for opening and closing the scroll
+
+    private void Start() {
+        playerCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        if (Application.isEditor) {
+            if (dontTriggerAtStart) {
+                scrollUI.SetActive(false);
+            }
+        }
+        if (Application.isEditor){
+            if (!dontTriggerAtStart) {
+                scrollUI.SetActive(true);
+            }
+        }
+    }
+    
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit scrollHit = CastRay(scrollLayer);
+            if (scrollHit.collider != null) {
+                scrollUI.SetActive(true);
+                freezeCam.DeactivateNoise();
+            }
+        }
+    }
+    private RaycastHit CastRay(LayerMask layer) {
+        Vector3 screenMousePosFar = new Vector3(
+            Input.mousePosition.x, 
+            Input.mousePosition.y, 
+            playerCamera.farClipPlane);        
+        Vector3 screenMousePosNear = new Vector3(
+            Input.mousePosition.x, 
+            Input.mousePosition.y, 
+            playerCamera.nearClipPlane);
+        Vector3 worldMousePosFar = playerCamera.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 worldMousePosNear = playerCamera.ScreenToWorldPoint(screenMousePosNear);
+        RaycastHit hit;
+        Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, Mathf.Infinity, layer);
+
+        return hit;
+    }
+}
