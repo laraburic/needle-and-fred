75b00e671a6b910fde4ea2c621b60ee9ed94f4eb
Mike O <mike413318@gmail.com>
2024-05-26 15:30:51 +0100

    Added Random SFX

--- Needle and Fred/Assets/Scripts/AudioManager.cs
+++ Needle and Fred/Assets/Scripts/AudioManager.cs
@@ -1,14 +1,18 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
+    public Sound[] rSounds;
     public AudioMixer mixer;
 
-    
     public static bool pause;
+
+    private System.Random random = new System.Random();
 
     void Awake()
     {
@@ -20,6 +24,32 @@ public class AudioManager : MonoBehaviour
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+        }
+
+        foreach (Sound s in rSounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        StartCoroutine(PlayRandomSounds());
+    }
+
+    IEnumerator PlayRandomSounds()
+    {
+        while (!pause)
+        {
+            float randomDelay = Random.Range(5f, 15f); //Adjust the delay between sounds as needed
+            yield return new WaitForSeconds(randomDelay);
+
+            int randomIndex = random.Next(rSounds.Length);
+            Sound randomSound = rSounds[randomIndex];
+            randomSound.source.Play();
+
+            Debug.Log($"Played random sound: {randomSound.name}");
         }
     }
 
