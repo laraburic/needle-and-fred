3f98dc6780f1d894a0e52013a4132f64efad3087
Mike O <mike413318@gmail.com>
2024-05-26 11:41:17 +0100

    Update MainMenu.cs

--- Needle and Fred/Assets/Scripts/MainMenu.cs
+++ Needle and Fred/Assets/Scripts/MainMenu.cs
@@ -9,7 +9,8 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         Debug.Log("Game Start");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void PlayWitch()
